03a68d793d28e36481cfdcef189c5672439550c7
iaxx <iaxx233@gmail.com>
2023-02-04 07:10:58 +0200

    string search within folders

---
+++ Search for a string inside a folder with many txt files.ps1
@@ -0,0 +1,6 @@
+﻿$folder = Read-Host -Prompt 'path to folder'
+$string = Read-Host -Prompt 'string to match'
+Get-ChildItem -Path $folder -Filter *.txt | ForEach-Object {
+    $file = $_
+    Select-String -Path $file.FullName -Pattern $string -AllMatches | Select-Object -Property Filename, Line, @{Name="File"; Expression={$file.Name}}
+}
